8672b92838eaa45c6742f1c5942e86b538074964
Seah Ying Hang <seahyinghang8@Seahs-MBP.hsd1.ca.comcast.net>
2020-09-20 23:06:42 -0700

    basic parser

---
+++ parser.py
@@ -0,0 +1,38 @@
+from typing import List, Tuple
+
+import pytesseract
+import pdf2image
+import pandas as pd
+
+try:
+    from PIL import Image
+except ImportError:
+    import Image
+
+
+def process_image(image: Image):
+    tesseract_out = pytesseract.image_to_data(image)
+    print(parse_tesseract_data(tesseract_out))
+
+
+def parse_tesseract_data(str) -> pd.DataFrame:
+    # first line is a header, last line is empty
+    lines = str.split('\n')[1:-1]
+    lines = [line.split('\t') for line in lines]
+    lines = [(line[11], line[10], line[6],
+              line[7], line[8], line[9]) for line in lines]
+    word, confidence, left, top, width, height = zip(*lines)
+    df = pd.DataFrame({
+        'word': word,
+        'confidence': confidence,
+        'left': left,
+        'top': top,
+        'width': width,
+        'height': height
+    })
+    return df
+
+
+if __name__ == "__main__":
+    pdf_path = 'data/b.pdf'
+    process_image(pdf2image.convert_from_path(pdf_path)[0])
