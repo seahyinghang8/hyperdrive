bac395a339b09d35d3922e7579c37df9bc3b168e
Girish Kumar <girishbarca@hotmail.com>
2020-10-15 17:12:06 -0700

    Simple Rule Generation

--- tools/hyperwidget/README.md
+++ tools/hyperwidget/README.md
@@ -6,26 +6,29 @@ A Custom Jupyter Widget Library
 Installation
 ------------
 
-## To install use pip:
+To install use pip:
 
     $ pip install hyperwidget
     $ jupyter nbextension enable --py --sys-prefix hyperwidget
 
-## Local Dev Installation for Classic Notebook
+To install for jupyterlab
 
-To develop this package against the classic notebook, run:
+    $ jupyter labextension install hyperwidget
 
-- `pip install -e .` (installs python package for development, runs `npm install` and `npm run build`)
-- `jupyter nbextension install --py --symlink --sys-prefix <python_package_name>`\
-(symlinks `static/` directory into `<jupyter path>/nbextensions/<extension_name>/`). Now the notebook has access to the frontend code.
-- `jupyter nbextension enable --py --sys-prefix <python_package_name>`\
-(copies `<npm_package_name>.json` into  `<environment path>/etc/jupyter/nbconfig/notebook.d/` directory). Now the notebook will load your frontend code on page load.
+For a development installation (requires npm),
 
-Now make some changes to your source code. Then:
+    $ git clone https://github.com//hyperwidget.git
+    $ cd hyperwidget
+    $ pip install -e .
+    $ jupyter nbextension install --py --symlink --sys-prefix hyperwidget
+    $ jupyter nbextension enable --py --sys-prefix hyperwidget
+    $ jupyter labextension install js
 
-- After making Python code changes, restarting the notebook kernel will be enough to reflect changes
-- After making JavaScript code changes:
-    - `cd js`
-    - `npm run build`
-    - Refresh browser to reflect changes
+When actively developing your extension, build Jupyter Lab with the command:
+
+    $ jupyter lab --watch
+
+This takes a minute or so to get started, but then automatically rebuilds JupyterLab when your javascript changes.
+
+Note on first `jupyter lab --watch`, you may need to touch a file to get Jupyter Lab to open.
 
---
+++ utils/rule_gen.py
@@ -0,0 +1,76 @@
+import itertools
+import spacy
+from spacy.lang.en import English
+
+from typing import List, Tuple, Dict
+
+from models.spatial_text import Page, Line
+from models.document import Document
+
+from utils.nlu import get_entity_scores
+from utils.extract import get_nearby_words
+
+def generate_query(
+    line: Line, page: Page, nlp: English = None
+) -> Dict:
+    if not nlp:
+        nlp = spacy.load("en_core_web_sm")
+    query = {
+        "name": "Extracted Field",
+        "arguments": {
+            "x-position": 0.0,
+            "y-position": 0.0,
+            "entity": None,
+            "word-neighbors": [],
+            "word-neighbor-top-thres": 0.05,
+            "word-neighbor-left-thres": 0.1,
+        },
+        "weights": {
+            "x-position": 0.5,
+            "y-position": 0.2,
+            "entity": 0.5,
+            "word-neighbors": 0.2,
+        }
+    }
+    query["arguments"]["entity"] = _gen_entity(line, nlp)
+    x_pos, y_pos = _gen_position(line, page)
+    query["arguments"]["x-position"] = x_pos
+    query["arguments"]["y-position"] = y_pos
+    query["arguments"]['word-neighbors'] = _gen_word_neighbors(
+        line, page)
+    return query
+
+def _gen_entity(
+    line: Line,
+    nlp: English
+) -> str:
+    doc = nlp(str(line))
+    entity_scores = get_entity_scores(nlp, doc)
+    max_score = 0
+    best_label = ""
+    for key in entity_scores:
+        entity_score = entity_scores[key]
+        _, _, cur_label = key
+        if entity_score > max_score:
+            max_score = entity_score
+            best_label = cur_label
+    return best_label
+
+def _gen_position(
+    line: Line,
+    page: Page
+) -> Tuple[float, float]:
+    return (
+        line.center_left / page.width,
+        line.center_top / page.height
+    )
+
+def _gen_word_neighbors(
+    line: Line,
+    page: Page,
+    top_thres: float = 0.05,
+    left_thres: float = 0.1,
+) -> List[str]:
+    line_idxs = get_nearby_words(line, page, top_thres, left_thres)
+    line_strs = [str(page.lines[i]).split(' ') for i in line_idxs]
+    return list(itertools.chain(*line_strs))
